20749edbcef4ab783a4512b6075691bc39b08bdd
PavlenkoIGOR <illusiopard@gmail.com>
2025-06-11 17:50:27 +0300

    hero ExplosionAnim added

--- Assets/Scripts/HeroController/Hero.cs
+++ Assets/Scripts/HeroController/Hero.cs
@@ -1,25 +1,49 @@
 using SpaceShooter;
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class Hero : Destructible
 {
     [SerializeField] private Animator _animExplosion;
-    private void Update()
+
+    [SerializeField]  private GameObject _heroExplosionView;
+
+    protected override void OnDeath()
     {
-        ShipExlosionAnim();
-    }
-    private void ShipExlosionAnim()
-    {
-        if (currentHitPoints <= 0)
+        Player.instance.AddScore(scoreValue);
+
+        foreach (Transform item in transform)
         {
-            _animExplosion.Play("HeroExplosionAnimation");
+            item.gameObject.SetActive(false);
         }
+        _heroExplosionView.SetActive(true);
+        StartCoroutine(PLayHeroExplosion());
     }
 
     protected override void OnDestroy()
-    {
+    {        
         _animExplosion.StopPlayback();
     }
 
+    private IEnumerator PLayHeroExplosion()
+    {
+        {
+            _animExplosion.Play("HeroExplosionAnimation");
+
+            float clipLength = default;
+            RuntimeAnimatorController rac = _animExplosion.runtimeAnimatorController;
+            for (int i = 0; i < rac.animationClips.Length; i++)
+            {
+                if (rac.animationClips[i].name == "HeroExplosionAnimation")
+                {
+                    clipLength = rac.animationClips[i].length;
+                }
+            }
+
+            yield return new WaitForSeconds(clipLength);
+
+            Destroy(gameObject);
+        }
+    }
 }
